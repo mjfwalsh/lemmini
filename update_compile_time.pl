a99d2fbdee2e3143575e3f31ea7e717a3de3df6d
Michael J. Walsh <mjfwalsh@gmail.com>
2019-10-15 15:41:42 +0100

    strftime isn't very portable

--- update_compile_time.pl
+++ update_compile_time.pl
@@ -1,22 +1,31 @@
 #!/usr/bin/perl
 
-use POSIX qw(strftime);
+use Time::localtime;
+use strict;
 
-my $data_stamp = strftime("%-d %B %Y", localtime(time));
+print "Updating Compile Datastamp...";
 
-print "Updating Compile Datastamp to $data_stamp...";
+my @months = qw|January February March April May June July August September October November December|;
 
-open(IN, '<', 'Game/Core.java') || fail();
-$txt = join '', <IN>;
-close IN;
+my $t = localtime(time);
 
-$txt =~ s|private static String releaseString *= *"[^"]*";|private static String releaseString = "Compiled on $data_stamp";| || fail();
+my $data_stamp = $t->mday . ' ' . $months[$t->mon] . ' ' . ($t->year + 1900);
 
-open(OUT, '>', 'Game/Core.java') || fail();
-print OUT $txt;
-close OUT;
+open(my $IN, '<', 'Game/Core.java') || fail();
+my $txt = join '', <$IN>;
+close $IN;
+
+$txt =~ s/private static String releaseString *= *"([^"]*)";/private static String releaseString = "Compiled on $data_stamp";/ || fail();
+
+if($1 eq "Compiled on $data_stamp") {
+	print "no need\n";
+	exit;
+}
+
+open(my $OUT, '>', 'Game/Core.java') || fail();
+print $OUT $txt;
+close $OUT;
 print "done.\n";
-
 
 sub fail {
 	print "failed.\n";
